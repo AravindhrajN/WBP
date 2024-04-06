8a31adcdd857b259549814e9dddacea564df6ed6
aravindhraj <smkaravindh@gmail.com>
2024-04-06 09:25:17 +0530

    commit 16

--- src/main/java/com/example/demo/Controller/UserController.java
+++ src/main/java/com/example/demo/Controller/UserController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.Entity.Faceproducts;
+import com.example.demo.Model.Product;
 import com.example.demo.services.service;
 
 @RestController
@@ -23,6 +25,13 @@ public class UserController {
 	public List<Faceproducts> getfproduct() {
 
 		return service.getproduct("face");
+
+	}
+
+	@GetMapping("getproduct")
+	public List<Product> getproduct(@RequestParam("id") long id, @RequestParam("product") String name) {
+
+		return service.gettingproduct(id, name);
 
 	}
 
